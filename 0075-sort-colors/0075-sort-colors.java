f3134f0f5f2ee74001420bb096711376adfc93e6
AsthaB05 <asthabavistale3@gmail.com>
2026-07-09 23:45:15 +0530

    Time: 0 ms (100%), Space: 43.6 MB (42.92%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count=new int[3];        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                count[0]++;
+            }else if(nums[i]==1){
+                count[1]++;
+            }else{
+                count[2]++;
+            }
+        }
+        int end=count[0];
+        int idx=0;
+        while(idx<nums.length){
+            if(idx<count[0]){
+                nums[idx]=0;
+            }else if(idx<(count[0]+count[1])){
+                nums[idx]=1;
+            }else{
+                nums[idx]=2;
+            }
+            idx++;
+        }
+    }
+}
