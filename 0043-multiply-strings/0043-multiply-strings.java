d894892efe17b68723144d355d88692e86faf3ce
AsthaB05 <asthabavistale3@gmail.com>
2026-07-22 00:32:54 +0530

    Time: 3 ms (82.65%), Space: 43.7 MB (37.47%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,74 +1,27 @@
 class Solution {
-
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) {
+         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
-        String[] arr = new String[num1.length()];
-        int idx = 0;
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            int digit1 = num1.charAt(i) - '0';
-            int carry = 0;
-            StringBuilder sb = new StringBuilder();
-            for (int j = num2.length() - 1; j >= 0; j--) {
+        int[] result = new int[num1.length() + num2.length()];
+        for (int i = num1.length()- 1; i >= 0; i--) {
+            for (int j = num2.length()- 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
 
-                int digit2 = num2.charAt(j) - '0';
+                int sum = mul + result[i + j + 1];
 
-                int mul = digit1 * digit2 + carry;
-
-                sb.append(mul % 10);
-                carry = mul / 10;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
             }
+        }
+        StringBuilder ans = new StringBuilder();
 
-            if (carry > 0) {
-                sb.append(carry);
+        for (int num : result) {
+            if (!(ans.length() == 0 && num == 0)) {
+                ans.append(num);
             }
-
-            sb.reverse();
-            for (int z = 0; z < idx; z++) {
-                sb.append('0');
-            }
-
-            arr[idx] = sb.toString();
-            idx++;
         }
 
-        String ans = "0";
-
-        for (String s : arr) {
-            ans = addStrings(ans, s);
-        }
-
-        return ans;
-    }
-
-    public String addStrings(String a, String b) {
-
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-
-        int carry = 0;
-
-        StringBuilder sb = new StringBuilder();
-
-        while (i >= 0 || j >= 0 || carry > 0) {
-
-            int sum = carry;
-
-            if (i >= 0) {
-                sum += a.charAt(i) - '0';
-                i--;
-            }
-
-            if (j >= 0) {
-                sum += b.charAt(j) - '0';
-                j--;
-            }
-
-            sb.append(sum % 10);
-            carry = sum / 10;
-        }
-
-        return sb.reverse().toString();
+        return ans.toString();
     }
 }
