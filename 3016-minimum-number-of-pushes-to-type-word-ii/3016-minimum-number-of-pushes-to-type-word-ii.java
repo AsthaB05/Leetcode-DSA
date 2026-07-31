67f4ef8ffe6fd25768f7ab132aa42ba4f47f87a8
AsthaB05 <asthabavistale3@gmail.com>
2026-07-31 15:48:48 +0530

    Time: 8 ms (99.71%), Space: 48.3 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumPushes(String word) {
+        if(word.length()<=8){
+            return word.length();
+        }
+        int[] freq=new int[26];
+        for(int i=0; i<word.length(); i++){
+            freq[word.charAt(i)-'a']++;
+        }
+        Arrays.sort(freq);
+        int res=0;
+        for(int i=25; i>=0; i--){
+            if(i>=18){
+                res+=freq[i];
+            }else if(i>=10){
+                res+=freq[i]*2;
+            }else if(i>=2){
+                res+=freq[i]*3;
+            }else{
+                res+=freq[i]*4;
+            }
+        }
+        return res;
+    }
+}
