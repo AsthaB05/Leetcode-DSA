10e2a3da546351661bd5a2ac57f763b015783b72
AsthaB05 <asthabavistale3@gmail.com>
2026-08-29 11:28:17 +0530

    Time: 17 ms (22.3%), Space: 46.3 MB (65.14%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int len1=0;
+        int count1=0;
+        int low1=0;
+        int high1=0;
+        while(high1<answerKey.length()){
+            if(answerKey.charAt(high1)=='F'){
+                count1++;
+            }
+            while(count1>k){
+                if(answerKey.charAt(low1)=='F'){
+                    count1--;
+                }
+                low1++;
+            }
+            len1=Math.max(len1,high1-low1+1);
+            high1++;
+
+        }
+        int len2=0;
+        int count2=0;
+        int low2=0;
+        int high2=0;
+        while(high2<answerKey.length()){
+            if(answerKey.charAt(high2)=='T'){
+                count2++;
+            }
+            while(count2>k){
+                if(answerKey.charAt(low2)=='T'){
+                    count2--;
+                }
+                low2++;
+            }
+            len2=Math.max(len2,high2-low2+1);
+            high2++;
+
+        }
+        return Math.max(len1,len2);
+
+    }
+}
