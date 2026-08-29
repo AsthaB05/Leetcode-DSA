23fb01ac531737d1e04683c0b855aeb0db0323a6
AsthaB05 <asthabavistale3@gmail.com>
2026-08-29 11:36:24 +0530

    Time: 12 ms (80.85%), Space: 46.3 MB (82.29%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -2,15 +2,20 @@ class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         int len1=0;
         int count1=0;
+        int count2=0;
         int low1=0;
         int high1=0;
         while(high1<answerKey.length()){
             if(answerKey.charAt(high1)=='F'){
                 count1++;
+            }else{
+                count2++;
             }
-            while(count1>k){
+            while(Math.min(count1,count2)>k){
                 if(answerKey.charAt(low1)=='F'){
                     count1--;
+                }else{
+                    count2--;
                 }
                 low1++;
             }
@@ -18,25 +23,26 @@ class Solution {
             high1++;
 
         }
-        int len2=0;
-        int count2=0;
-        int low2=0;
-        int high2=0;
-        while(high2<answerKey.length()){
-            if(answerKey.charAt(high2)=='T'){
-                count2++;
-            }
-            while(count2>k){
-                if(answerKey.charAt(low2)=='T'){
-                    count2--;
-                }
-                low2++;
-            }
-            len2=Math.max(len2,high2-low2+1);
-            high2++;
+        return len1;
+        // int len2=0;
+        // int count2=0;
+        // int low2=0;
+        // int high2=0;
+        // while(high2<answerKey.length()){
+        //     if(answerKey.charAt(high2)=='T'){
+        //         count2++;
+        //     }
+        //     while(count2>k){
+        //         if(answerKey.charAt(low2)=='T'){
+        //             count2--;
+        //         }
+        //         low2++;
+        //     }
+        //     len2=Math.max(len2,high2-low2+1);
+        //     high2++;
 
-        }
-        return Math.max(len1,len2);
+        // }
+        // return Math.max(len1,len2);
 
     }
 }
