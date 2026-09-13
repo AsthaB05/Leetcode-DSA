b76cfd95368d0ba045a180c6af13c7774e96fdb6
AsthaB05 <asthabavistale3@gmail.com>
2026-09-14 00:28:37 +0530

    Time: 40 ms (35.25%), Space: 88.4 MB (48.66%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.java
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.java
@@ -1,33 +1,31 @@
 class Solution {
     public int[] getStrongest(int[] arr, int k) {
-        if(arr.length<=1){
-            return arr;
-        }
+        // if(arr.length<=1){
+        //     return arr;
+        // }
         Arrays.sort(arr);
         int m=arr[(arr.length-1)/2];
         int left=0;
         int right=arr.length-1;
-        ArrayList<Integer> list=new ArrayList<>();
-        while(left<=right && list.size()<=k){
+        int[] res=new int[k];
+        int idx=0;
+        while(idx<k){
             if(Math.abs(arr[left]-m)>Math.abs(arr[right]-m)){
-                list.add(arr[left]);
+                res[idx]=arr[left];
                 left++;
             }else if(Math.abs(arr[left]-m)<Math.abs(arr[right]-m)){
-                list.add(arr[right]);
+                res[idx]=arr[right];
                 right--;
             }else{
                 if(arr[right]>arr[left]){
-                    list.add(arr[right]);
+                    res[idx]=arr[right];
                     right--;
                 }else{
-                    list.add(arr[left]);
+                    res[idx]=arr[left];
                     left++;
                 }
             }
-        }
-        int[] res=new int[k];
-        for(int i=0; i<k; i++){
-            res[i]=list.get(i);
+            idx++;
         }
         return res;
     }
