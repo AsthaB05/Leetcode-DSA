9b34d05a607257d8c9fc36acc77a341e8fad5b57
AsthaB05 <asthabavistale3@gmail.com>
2026-09-21 13:39:16 +0530

    Time: 4 ms (29.31%), Space: 94.8 MB (90.31%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.java
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.java
@@ -3,12 +3,12 @@ class Solution {
         long[] cover=new long[nums.length];
         cover[0]=nums[0]*2;
         int max=nums[0];
+        int sum=0;
         for(int i=1; i<nums.length; i++){
             max=Math.max(max, nums[i]);
-            cover[i]=max+nums[i];
-        }
-        for(int i=1; i<cover.length; i++){
-            cover[i]+=cover[i-1];
+            sum=max+nums[i];
+            cover[i]=sum+cover[i-1];
+
         }
         return cover;
     }
